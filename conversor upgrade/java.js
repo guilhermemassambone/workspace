486bb2a74c89bb31dc59d7b744d0c433013acf10
guilhermemassambone <godoi.massambone@gmail.com>
2023-06-14 11:10:42 -0300

    Commit conversor upgrade finalizado

--- conversor upgrade/java.js
+++ conversor upgrade/java.js
@@ -11,46 +11,129 @@ buttonConvert.addEventListener("click", convert)
 moedaspara.addEventListener("change", changeSettingsPara)
 moedasde.addEventListener("change", changeSettingsDe)
 
-const dolarToday = 4.90
-const euroToday = 5.80
-const libraToday = 6.20
+const dolarToday = 4.85
+const euroToday = 5.26
+const libraToday = 6.16
 
 function convert(){
     const valorInput = document.querySelector(".valorDoInput").value
 
-    if(moedaspara.value == "dolar"){
+    if(moedasde.value == "realde"){
         moedaOrigem.innerHTML = new Intl.NumberFormat("pt-BR",{
             style: "currency",
             currency: "BRL"    
         }).format(valorInput)
-        moedaFinal.innerHTML = new Intl.NumberFormat("en-US",{
-            style: "currency",
-            currency: "USD"    
-        }).format(valorInput / dolarToday)
-    }
-
-    if(moedaspara.value == "euro"){
-        moedaOrigem.innerHTML = new Intl.NumberFormat("pt-BR",{
-            style: "currency",
-            currency: "BRL"    
-        }).format(valorInput)
-        moedaFinal.innerHTML = new Intl.NumberFormat("de-DE",{
-            style: "currency",
-            currency: "EUR"    
-        }).format(valorInput / euroToday)
-    }
-
-    if(moedaspara.value == "libra"){
-        moedaOrigem.innerHTML = new Intl.NumberFormat("pt-BR",{
-            style: "currency",
-            currency: "BRL"    
-        }).format(valorInput)
-        moedaFinal.innerHTML = new Intl.NumberFormat("en-UK",{
-            style: "currency",
-            currency: "GBP"    
-        }).format(valorInput / libraToday)
+        if(moedaspara.value == "dolar"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("en-US",{
+                style: "currency",
+                currency: "USD"    
+            }).format(valorInput / dolarToday)
+        } else if(moedaspara.value == "euro"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("de-DE",{
+                style: "currency",
+                currency: "EUR"    
+            }).format(valorInput / euroToday)
+        } else if(moedaspara.value == "libra"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("en-UK",{
+                style: "currency",
+                currency: "GBP"    
+            }).format(valorInput / libraToday)
+        } else if(moedaspara.value == "real"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("pt-BR",{
+                style: "currency",
+                currency: "BLR"    
+            }).format(valorInput)
+        }
     }
     
+    if(moedasde.value == "dolarde"){
+        moedaOrigem.innerHTML = new Intl.NumberFormat("en-US",{
+            style: "currency",
+            currency: "USD"    
+        }).format(valorInput)
+        if(moedaspara.value == "real"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("pt-BR",{
+                style: "currency",
+                currency: "BLR"    
+            }).format(valorInput * dolarToday)
+        } else if(moedaspara.value == "euro"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("de-DE",{
+                style: "currency",
+                currency: "EUR"    
+            }).format(valorInput * 0.92)
+        } else if(moedaspara.value == "libra"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("en-UK",{
+                style: "currency",
+                currency: "GBP"    
+            }).format(valorInput * 0.79)
+        } else if(moedaspara.value == "dolar"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("en-US",{
+                style: "currency",
+                currency: "USD"    
+            }).format(valorInput)
+        }
+    }
+
+    if(moedasde.value == "eurode"){
+        moedaOrigem.innerHTML = new Intl.NumberFormat("de-DE",{
+            style: "currency",
+            currency: "EUR"    
+        }).format(valorInput)
+        if(moedaspara.value == "real"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("pt-BR",{
+                style: "currency",
+                currency: "BLR"    
+            }).format(valorInput * euroToday)
+        } else if(moedaspara.value == "dolar"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("en-US",{
+                style: "currency",
+                currency: "USD"    
+            }).format(valorInput * 1.08)
+        } else if(moedaspara.value == "libra"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("en-UK",{
+                style: "currency",
+                currency: "GBP"    
+            }).format(valorInput * 0.85)
+        } else if(moedaspara.value == "euro"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("de-DE",{
+                style: "currency",
+                currency: "EUR"    
+            }).format(valorInput)
+        }
+    }
+
+    if(moedasde.value == "librade"){
+        moedaOrigem.innerHTML = new Intl.NumberFormat("en-UK",{
+            style: "currency",
+            currency: "GBP"    
+        }).format(valorInput)
+        if(moedaspara.value == "real"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("pt-BR",{
+                style: "currency",
+                currency: "BLR"    
+            }).format(valorInput * libraToday)
+        } else if(moedaspara.value == "euro"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("de-DE",{
+                style: "currency",
+                currency: "EUR"    
+            }).format(valorInput * 1.17)
+        } else if(moedaspara.value == "dolar"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("en-US",{
+                style: "currency",
+                currency: "USD"    
+            }).format(valorInput * 1.27)
+        } else if(moedaspara.value == "real"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("pt-BR",{
+                style: "currency",
+                currency: "BLR"    
+            }).format(valorInput)
+        } else if(moedaspara.value == "libra"){
+            moedaFinal.innerHTML = new Intl.NumberFormat("en-UK",{
+                style: "currency",
+                currency: "GBP"    
+            }).format(valorInput)
+        }
+    }
     
 }
 
@@ -76,6 +159,7 @@ function changeSettingsPara(){
     if(moedaspara.value == "real"){
         flagfinal.src = "./assets/brasil.png"
         textMoedaFinal.innerHTML = "Real"
+        convert()
     }
 
 }
